3ba6352b91398188010045e783e33bd0f28d58d4
Chris <cryptophoto@gmail.com>
2024-05-23 22:17:31 +1000

    inline the #define TEMPERATURE option

--- src/info.cpp
+++ src/info.cpp
@@ -90,10 +90,12 @@ void Info::print_initialize(LBM* lbm) {
 #ifdef SURFACE
 	println("| Surface Tension | "+alignr(57u, /**********************************************************************************/ to_string(lbm->get_sigma(), 8u))+" |");
 #endif // SURFACE
-#ifdef TEMPERATURE
+//cnd #ifdef TEMPERATURE
+	if(g_args["TEMPERATURE"].as<bool>()) {
 	println("| Thermal Diff.   | "+alignr(57u, /**********************************************************************************/ to_string(lbm->get_alpha(), 8u))+" |");
 	println("| Thermal Exp.    | "+alignr(57u, /***********************************************************************************/ to_string(lbm->get_beta(), 8u))+" |");
-#endif // TEMPERATURE
+	}
+//cnd #endif // TEMPERATURE
 #ifndef INTERACTIVE_GRAPHICS_ASCII
 	println("|---------.-------'-----.-----------.-------------------.---------------------|");
 	println("| MLUPs   | Bandwidth   | Steps/s   | Current Step      | "+string(steps==max_ulong?"Elapsed Time  ":"Time Remaining")+"      |");
